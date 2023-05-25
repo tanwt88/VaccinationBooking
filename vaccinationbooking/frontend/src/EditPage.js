8551418e87f8c5961bf4af36036c13df4af7bae0
tanwt88 <weitian88.wtt@gmail.com>
2023-05-25 13:54:02 +0800

    created server.js

---
+++ vaccinationbooking/frontend/src/EditPage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const EditPage = () => {
+  return (
+    <div>
+    <label for="name">Name:</label>
+    <input type = "text" id="name" value= "name" onChange={e=>setName(e.target.value)}/>
+    <label for="nric">NRIC:</label>
+    <input type = "text" value= "nric" onChange={e=>setNRIC(e.target.value)}/>
+
+    <select id = "jabsnumber">
+    <option value="firstjab">1st Jab</option>
+    <option value="secondjab">2nd Jab</option>
+    </select>
+
+    <select id = "edit">
+    <option value="appointment">`${appt.timeslot} at ${appt.venue}, ${appt.type} ${appt.jabsnumber}`</option>
+    </select>
+
+
+    </div>
+
+  )
+}
+
+export default EditPage
